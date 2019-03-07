2416b2ff83def3955ea5174d77b6da18f91eefe8
Jesus Delgado <eldelgado6@gmail.com>
2019-03-07 14:31:56 +0100

    añadido archivo config.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-//const config = require('./config');
+const config = require('./config');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/vivero',{useNewUrlParser:true})
+mongoose.connect(config.db_uri,{useNewUrlParser:true})
 .then(db => console.log('Conexión correcta a la BD'))
 .catch(err => console.log('Error en la conexión a la BD'));
 
@@ -22,4 +22,4 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(morgan('dev'));
 
 // --- PUERTO DE ESCUCHA
-app.listen(3000, () => console.log('Servidor iniciado en el puerto 3000'));
+app.listen(config.port, () => console.log('Servidor iniciado en el puerto 3000'));
